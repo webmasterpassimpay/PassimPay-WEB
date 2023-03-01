27954bfb7d7baecbe264b6e354cf9f5326ff5dd6
andreiyaruk <84790529+andreiyaruk@users.noreply.github.com>
2023-03-01 13:45:54 +0300

    update

---
+++ js/account-exchange.js
@@ -0,0 +1,178 @@
+const transferBlocks = Array.from(document.querySelectorAll(".transfer-block"));
+const transferLinks = Array.from(document.querySelectorAll("._transfer-link"));
+const transferSteps = Array.from(
+  document.querySelectorAll(".transfer-block__step")
+);
+const transferBlockLinks = Array.from(
+  document.querySelectorAll(".transfer-block__link")
+);
+const transferBlockBlocks = Array.from(
+  document.querySelectorAll(".border_block")
+);
+const exchangeBlocks = Array.from(document.querySelectorAll(".exchange-block"));
+const exchangeBlocksBottom = Array.from(
+  document.querySelectorAll(".exchange-block__bottom")
+);
+const exchangeIcon = document.querySelector(".exchange-block__top-animation");
+const exchangeStatus = Array.from(
+  document.getElementsByClassName("exchange-block__top-status")
+);
+const exchangeBack = document.querySelector("#exchange-back");
+const exchangeId = document.querySelector("#exchange-id");
+const exchangeRight = document.querySelector("#withdraw_right");
+const exchangeLink = document.querySelector("._exchange-link");
+const exchangeLinkSecond = document.querySelector("._exchange-link-second");
+// Здесь тоже самое, что и в верстке - только для демоснтрации разных блоков
+if (exchangeLink) {
+  exchangeBlocks[0].style.display = "flex";
+  transferSteps[0].classList.add("active");
+  exchangeId.style.display = "none";
+
+  // Первая кнопка - Отправить
+  exchangeLink.onclick = () => {
+    exchangeBlocks[0].style.display = "none";
+    transferSteps[0].classList.remove("active");
+    transferSteps[1].classList.add("active");
+    exchangeBlocks[1].style.display = "block";
+    exchangeBlocksBottom[0].style.display = "block";
+  };
+  // Кнопка для демонстрации верстки
+  exchangeLinkSecond.addEventListener("click", exchangeClick);
+  let index = 0;
+  let exchangeStatusText = "Ожидание депозита";
+  function exchangeClick(e) {
+    exchangeBlocksBottom[index].style.display = "none";
+    exchangeBlocksBottom[index + 1].style.display = "block";
+    transferSteps[index + 1].classList.remove("active");
+    transferSteps[index + 2].classList.add("active");
+    index += 1;
+
+    switch (index) {
+      // Здесб провряем счетчик слайда и добавляем нужный контент
+      case 0:
+        break;
+      case 1:
+        exchangeStatusText = "Подтверждение депозита";
+        exchangeRight.style.display = "none";
+        exchangeId.style.display = "flex";
+        exchangeStatus[0].classList.add("loading");
+        transferSteps[1].style.display = "none";
+        break;
+      case 2:
+        exchangeStatus[0].classList.remove("loading");
+        exchangeStatus[0].classList.add("success");
+        exchangeStatusText = "Завершено";
+        exchangeIcon.classList.remove("animation-playing");
+        exchangeIcon.classList.remove("follow-the-leader");
+        exchangeIcon.innerHTML = `
+          <img src="svg/success.svg" alt="">
+        `;
+        break;
+
+      default:
+        break;
+    }
+    exchangeStatus[0].innerHTML = exchangeStatusText;
+  }
+
+  // Кнопка вернуться на первый экран "Обмен валют"
+
+  exchangeBack.onclick = () => {
+    exchangeBlocksBottom[index].style.display = "none";
+    exchangeId.style.display = "none";
+    exchangeIcon.innerHTML = `
+    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    `;
+    exchangeIcon.classList.add("animation-playing");
+    exchangeIcon.classList.add("follow-the-leader");
+    exchangeBlocks[1].style.display = "none";
+    exchangeStatus[0].classList.remove("success");
+    exchangeRight.style.display = "block";
+    exchangeBlocks[0].style.display = "block";
+    exchangeStatus[0].innerHTML = "Ожидание депозита";
+    transferSteps[1].style.display = "block";
+    transferSteps[0].classList.add("active");
+    transferSteps[3].classList.remove("active");
+    index = 0;
+  };
+}
+// Общие стили для переключения табов
+function nav(blocks, links, activeClass, secondClass = "") {
+  if (links[0]) {
+    links[0].classList.add(activeClass);
+    blocks[0].style.display = "block";
+
+    links.forEach((link) => {
+      link.addEventListener("click", toggleActiveBlock);
+    });
+
+    function toggleActiveBlock() {
+      hideAllBlock(links, blocks);
+      let index = links.indexOf(this);
+      this.classList.add(activeClass);
+      blocks[index].style.display = "block";
+
+      links.forEach((link) => {
+        if (link != this) {
+          link.classList.add(secondClass);
+        }
+      });
+      if (
+        document
+          .querySelector(".transfer-block__link")
+          .classList.contains("border_title")
+      ) {
+        document.querySelector("#withdraw-info-first").style.display = "grid";
+        document.querySelector("#withdraw-info-second").style.display = "none";
+      } else {
+        document.querySelector("#withdraw-info-second").style.display = "block";
+        document.querySelector("#withdraw-info-first").style.display = "none";
+      }
+    }
+
+    function hideAllBlock(linksArr, blocksArr) {
+      linksArr.forEach((link) => {
+        link.classList.remove(activeClass);
+      });
+      blocksArr.forEach((block) => {
+        block.style.display = "none";
+      });
+    }
+  }
+}
+
+nav(transferBlocks, transferLinks, "active");
+nav(
+  transferBlockBlocks,
+  transferBlockLinks,
+  "border_title",
+  "exchange-block__item"
+);
+
+// Чекбокс и статус защищенности
+const checkBoxVpm = document.querySelector("#check-vpm");
+const vpmStatus = document.querySelector(".vpm-volatility__status");
+
+if (checkBoxVpm) {
+  let vpbStatusChildren = Array.from(vpmStatus.children);
+  vpbStatusChildren[0].classList.add("active");
+
+  checkBoxVpm.addEventListener("click", function (e) {
+    if (
+      e.target.classList.contains("checkbox-ios-switch") ||
+      e.target.classList.contains("checkbox-ios")
+    ) {
+      checkBoxVpm.classList.toggle("active");
+      vpbStatusChildren.forEach((el) => {
+        el.classList.toggle("active");
+      });
+    }
+  });
+}
+
+// slider
+const rangeValue = document.querySelector("#rangeValue");
+
+function rangeSlide(value) {
+  rangeValue.innerHTML = `${value}%`;
+}
---
+++ js/dashboard-popup.js
@@ -0,0 +1,246 @@
+// Слайд у кошельков
+$("body").delegate(".wallets__show", "click", function (event) {
+  if ($(event.target).closest(".wallets__btns").length) return;
+  $(this).toggleClass("active").next().slideToggle(300);
+  event.stopImmediatePropagation();
+});
+
+// Перемещение у кошельков
+const tasksListElement = Array.from(
+  document.querySelectorAll(`.wallets__slide`)
+);
+
+tasksListElement.forEach((element) => {
+  const taskElements = Array.from(
+    element.querySelectorAll(`.slide-wallets__input`)
+  );
+  for (const task of taskElements) {
+    task.draggable = true;
+  }
+  element.addEventListener(
+    `dragstart`,
+    (evt) => {
+      evt.target.classList.add(`selected`);
+      evt.stopImmediatePropagation();
+    },
+    true
+  );
+  element.addEventListener(
+    `dragend`,
+    (evt) => {
+      evt.target.classList.remove(`selected`);
+      evt.stopImmediatePropagation();
+    },
+    true
+  );
+  element.addEventListener(
+    `dragover`,
+    (evt) => {
+      // Разрешаем сбрасывать элементы в эту область
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = "move";
+      // Находим перемещаемый элемент
+      const activeElement = element.querySelector(`.selected`);
+      // Находим элемент, над которым в данный момент находится курсор
+      const currentElement = evt.target;
+      // Проверяем, что событие сработало:
+      // 1. не на том элементе, который мы перемещаем,
+      // 2. именно на элементе списка
+      const isMoveable =
+        activeElement !== currentElement &&
+        currentElement.classList.contains(`slide-wallets__input`);
+
+      // Если нет, прерываем выполнение функции
+      if (!isMoveable) {
+        return;
+      }
+
+      // Находим элемент, перед которым будем вставлять
+      const nextElement =
+        currentElement === activeElement.nextElementSibling
+          ? currentElement.nextElementSibling
+          : currentElement;
+
+      // Вставляем activeElement перед nextElement
+      let parent = activeElement.parentElement;
+      parent.insertBefore(activeElement, nextElement);
+      evt.stopImmediatePropagation();
+    },
+    true
+  );
+});
+
+const allWalletsDrag = document.querySelector(`.all-wallets__drag`);
+
+if (allWalletsDrag) {
+  const walletsItems = allWalletsDrag.querySelectorAll(`.wallets__item`);
+  for (const wallet of walletsItems) {
+    wallet.draggable = true;
+  }
+  allWalletsDrag.addEventListener(
+    `dragstart`,
+    (evt) => {
+      evt.target.classList.add(`selected`);
+      evt.stopImmediatePropagation();
+      evt.target.style.cursor = "move";
+    },
+    true
+  );
+
+  allWalletsDrag.addEventListener(
+    `dragend`,
+    (evt) => {
+      evt.target.classList.remove(`selected`);
+      evt.stopImmediatePropagation();
+      evt.target.style.cursor = "default";
+    },
+    true
+  );
+
+  allWalletsDrag.addEventListener(
+    `dragover`,
+    (evt) => {
+      // Разрешаем сбрасывать элементы в эту область
+      evt.preventDefault();
+
+      // Находим перемещаемый элемент
+      const activeElement = allWalletsDrag.querySelector(`.selected`);
+      // Находим элемент, над которым в данный момент находится курсор
+      const currentElement = evt.target.parentElement;
+      // Проверяем, что событие сработало:
+      // 1. не на том элементе, который мы перемещаем,
+      // 2. именно на элементе списка
+      const isMoveable =
+        activeElement !== currentElement &&
+        currentElement.classList.contains(`wallets__item`, "wallets__show");
+      // Находим элемент, перед которым будем вставлять
+      const nextElement =
+        currentElement === activeElement.nextElementSibling
+          ? currentElement.nextElementSibling
+          : currentElement;
+      // Вставляем activeElement перед nextElement
+      allWalletsDrag.insertBefore(activeElement, nextElement);
+      evt.stopImmediatePropagation();
+    },
+    true
+  );
+}
+
+// Ховер у попапа с кошельками
+const wallets = document.querySelector(".wallets");
+if (wallets) {
+  const walletsHover = document.querySelector(".wallets__hover");
+
+  if (window.screen.width > 993) {
+    hover(wallets, walletsHover);
+  } else {
+    click(wallets, walletsHover);
+  }
+}
+
+const walletsBtn = Array.from(document.querySelectorAll(".wallets__btn"));
+
+if (walletsBtn[0]) {
+  walletsBtn.forEach((btn) => {
+    hover(btn, btn);
+  });
+}
+
+// Добавление активных классов в изменении кошелька
+const settingsRowArr = Array.from(
+  document.querySelectorAll(".create-wallet__items")
+);
+
+if (settingsRowArr[0]) {
+  settingsRowArr.forEach((settingsRow) => {
+    let childArr = Array.from(settingsRow.children);
+
+    childArr.forEach((el) => {
+      el.onclick = function () {
+        removeAllActiveClass();
+        this.classList.add("active");
+      };
+    });
+    function removeAllActiveClass() {
+      childArr.forEach((el) => {
+        el.classList.remove("active");
+      });
+    }
+  });
+}
+
+// Перенос кнопки Структура комиссий
+
+const structureBtn = document.querySelector("#open_list_partners");
+
+if (structureBtn && window.screen.width < 993) {
+  const structureParentBlock = document.querySelector("#content-menu");
+  let cloneBtn = structureBtn.cloneNode(true);
+  structureBtn.style.display = "none";
+  cloneBtn.style.cssText = `
+    max-width: 324px;
+    margin: 0 auto;
+    margin-top: 20px;
+  `;
+  structureParentBlock.appendChild(cloneBtn);
+}
+
+function hover(parent, children) {
+  parent.addEventListener("mouseover", () => {
+    children.classList.add("active");
+  });
+  parent.addEventListener("mouseout", () => {
+    children.classList.remove("active");
+  });
+}
+
+function click(parent, children) {
+  parent.addEventListener(
+    "click",
+    (event) => {
+      if (
+        event.target.classList.contains("balance_total") ||
+        event.target.classList.contains("_wallets-img")
+      ) {
+        children.classList.toggle("active");
+        event.stopImmediatePropagation();
+      } else {
+        return;
+      }
+    },
+    { capture: true }
+  );
+}
+
+const walletsHoverParent = Array.from(
+  document.querySelectorAll(".wallets__show__hover")
+);
+
+if (walletsHoverParent && window.screen.width < 992) {
+  walletsHoverParent.forEach((el) => {
+    el.classList.add("wallets__show");
+  });
+}
+
+const slideWalletsInput = Array.from(
+  document.querySelectorAll(".slide-wallets__input__hover")
+);
+
+if (slideWalletsInput[0]) {
+  slideWalletsInput.forEach((el) => {
+    el.onclick = function (e) {
+      if (e.target.tagName == "INPUT") {
+        walletsHoverParent.forEach((el) => {
+          el.classList.remove("active");
+        });
+        this.classList.toggle("active");
+        this.parentElement.previousElementSibling.classList.add("active");
+        if (slideWalletsInput.every((el) => !el.classList.contains("active"))) {
+          console.log("123");
+
+          this.parentElement.previousElementSibling.classList.remove("active");
+        }
+      }
+    };
+  });
+}
